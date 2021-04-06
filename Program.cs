0a89a64591386740206dd63603d6bf63efb510b0
P4ul0Silva <paulos43@outlook.com>
2021-04-06 13:36:42 -0300

    calculadora_de_download_projeto

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DTimeCalculator
+{
+    class DTC
+    {
+
+        static void Main(String[] args)
+        {
+            
+            
+            
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//                                                                                      //");
+            Console.WriteLine("//       DDDDDD                TTTTTTTTTTTTTTT              CCCCCC                      //");
+            Console.WriteLine("//       DD    DD                    TT                   CC                            //");  
+            Console.WriteLine("//       DD     DD                   TT                   CC                            //");
+            Console.WriteLine("//       DD     DD                   TT                   CC                            //");
+            Console.WriteLine("//       DD    DD                    TT                   CC                            //");
+            Console.WriteLine("//       DDDDDD    Download          TT   Time              CCCCCC  Calculator          //");
+            Console.WriteLine("//                                                                            P4uloSilva//");
+            Console.WriteLine("//////////////////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Bem Vindo à Calculadora de Download DTC \nCalcule quanto tempo vai durar o seu Download:\n ");
+            Console.WriteLine("Digite a Velocidade da sua Conexão (em megabits ex: 10):\n" );
+            Console.WriteLine(" 1: Velocidade da sua Conexão(somente números): ...\n\n\nOu pressione X para fechar o programa.");
+            double downspeed = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nAgora o tamanho do Download em MB\nExemplo:\n     10, 999, 5000, 10500...\n");
+            Console.WriteLine("     |MB     |GB            ");
+            double downsize = double.Parse(Console.ReadLine());
+            double Dtime = downsize / (downspeed/8);
+            DateTime agora = DateTime.Now;
+            DateTime agora2 = agora.AddSeconds(Dtime);
+            TimeSpan interval = agora - agora2;
+            Console.WriteLine($"\nDuração de Download: {interval.ToString()} ");
+            Console.WriteLine($"\nSeu Download vai terminar às {agora2}");
+           
+
+            
+        
+            
+            
+            
+           
+        
+
+            
+
+        }
+
+    }
+}
