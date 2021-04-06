bce8d3fd876afac96d298ad038a231a0c9845724
P4ul0Silva <paulos43@outlook.com>
2021-04-06 15:22:22 -0300

    melhoria de uso implementando laço

--- Program.cs
+++ Program.cs
@@ -23,8 +23,11 @@ namespace DTimeCalculator
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Bem Vindo à Calculadora de Download DTC \nCalcule quanto tempo vai durar o seu Download:\n ");
-            Console.WriteLine("Digite a Velocidade da sua Conexão (em megabits ex: 10):\n" );
-            Console.WriteLine(" 1: Velocidade da sua Conexão(somente números): ...\n\n\nOu pressione X para fechar o programa.");
+            Console.WriteLine("Pressione 1 para Começar a Calcular ou X para Sair");
+            string opcao = Console.ReadLine();
+            while (opcao == "1")
+            {
+            Console.WriteLine(" 1: Velocidade da sua Conexão(somente números): ...\n\n\nOu pressione Enter para fechar o programa.");     
             double downspeed = double.Parse(Console.ReadLine());
             Console.WriteLine("\nAgora o tamanho do Download em MB\nExemplo:\n     10, 999, 5000, 10500...\n");
             Console.WriteLine("     |MB     |GB            ");
@@ -33,9 +36,20 @@ namespace DTimeCalculator
             DateTime agora = DateTime.Now;
             DateTime agora2 = agora.AddSeconds(Dtime);
             TimeSpan interval = agora - agora2;
-            Console.WriteLine($"\nDuração de Download: {interval.ToString()} ");
-            Console.WriteLine($"\nSeu Download vai terminar às {agora2}");
-           
+            Console.WriteLine($" ");
+            Console.WriteLine($"\nComeçando exatamente agora {DateTime.Now}...\nO seu Download de {downsize}MB, à uma velocidade de {downspeed}Mb/s com duração de: {interval.ToString()}\nIrá terminar às {agora2}\n\n\n");
+
+        
+
+            }
+
+            
+            
+
+            
+            if (opcao == "x"){
+                Console.WriteLine("Obrigado por usar meu programa! ;)\n                                   P4ul0Silva");
+            }
 
             
         
